cad2fcfd87063d912ca5ddd735a0d493e114a53a
Marjorie Roswell <mroswell@gmail.com>
2026-01-30 22:55:11 -0500

    Rename supabase client variable to db

--- js/config.js
+++ js/config.js
@@ -8,4 +8,4 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 // const SUPABASE_ANON_KEY = 'sb_publishable_RKe-giF2o95YB2CylBMUOA_P5_R7h3I';
 
 // Initialize Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
